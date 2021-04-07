37958f1e25e08e36a681a57b6afd6e68aa9bc57f
haselnuts <haselnuts@hotmail.com>
2021-04-07 09:36:27 +0000

    added flash message to contact.html

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 # 2 line space betwenn function needed ALWAYS, this is called PEP8 compliant
+# request needed for forms
+# flash needed for feedback to user, to provide a "flashed message"
+# need to create a secret key, because Flask cryptographically signs all messages for security purposes
+# need to create a .gitignore file, any file or folder will be igrnored by GitHub so we do not accidently commit files with sensitive information
+# also, we need to create a new py file called env.py, here we hide any sensitive information
+# if statement os.path.exists...  will only import env.py file if it exists -> new directory __pycache__ will be created after saving file
+
+
 
 app = Flask(__name__)
 # instance of the class Flask
 # flask needs this to look for templates and static files
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -45,8 +56,10 @@ def careers():
     return render_template("careers.html", page_title="Careers")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
